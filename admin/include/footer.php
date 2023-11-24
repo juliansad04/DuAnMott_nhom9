fd9c88f5d7470272f261c84b3e87100d75da40c2
devkhanh <huekha0.4@gmail.com>
2023-11-24 10:49:46 +0700

    N9_admin_04: footer

--- admin/include/footer.php
+++ admin/include/footer.php
@@ -1,10 +1,1 @@
-<<<<<<< HEAD
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap/dist/js/bootstrap.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-
-</html>
-=======
 <?php
->>>>>>> 3b038c6 (N9_admin_04: footer)
