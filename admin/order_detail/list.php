39763fd9f2b86517948d405ab5ee04d2b14bbcca
devkhanh <huekha0.4@gmail.com>
2023-11-24 10:53:12 +0700

    N9_admin_18: list_orderdetail

---
+++ admin/order_detail/list.php
@@ -0,0 +1,55 @@
+<?php
+if (is_array($listOrderDetail)) {
+    ?>
+    <div class="container-fluid" style="margin-left: 25px;">
+        <div class="card mt-12" style="width: 1450px;">
+            <div class="card-header d-flex align-items-center justify-content-between">
+                <h4>Chi tiết đơn hàng</h4>
+            </div>
+            <div class="card-body">
+                <table class="table table-bordered table-hover">
+                    <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Tên Sản phẩm</th>
+                        <th scope="col">Số lượng</th>
+                        <th scope="col">Đơn giá</th>
+                        <th scope="col">Thành tiền</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+
+                    <?php
+                    foreach ($listOrderDetail as $orderDetail) {
+                        echo "<tr>";
+                        echo "<td>" . $orderDetail['id'] . "</td>";
+
+                        $productId = $orderDetail['product_id'];
+                        $product = new Product();
+                        $productDetails = $product->getProductById($productId);
+                        $productName = ($productDetails) ? $productDetails['name'] : "Không xác định";
+                        echo "<td>" . $productName . "</td>";
+
+                        echo "<td>" . $orderDetail['quantity'] . "</td>";
+                        echo "<td>" . number_format($productDetails['price'], 2) . "</td>";
+
+                        $totalPrice = floatval($orderDetail['total_price']);
+                        echo "<td>" . number_format($totalPrice, 2) . "</td>";
+                        echo "</tr>";
+                    }
+
+                    ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    <?php
+} else {
+    echo "<div class='container mt-5'>";
+    echo "<div class='alert alert-info' role='alert'>";
+    echo "Không có chi tiết đơn hàng.";
+    echo "</div>";
+    echo "</div>";
+}
+?>
