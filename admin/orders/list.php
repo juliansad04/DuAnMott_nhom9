759c3612fb087043fb2a2e7ec779c0daf9b67f1c
devkhanh <huekha0.4@gmail.com>
2023-11-24 10:52:18 +0700

    N9_admin_14: list_orders

---
+++ admin/orders/list.php
@@ -0,0 +1,61 @@
+<div class="container-fluid" style="margin-left: 25px;">
+    <div class="card mt-12" style="width: 1450px;">
+        <div class="card-header d-flex align-items-center justify-content-between">
+            <h4>Danh sách đơn hàng</h4>
+            <a href="index.php?act=addorder" class="btn btn-success">THÊM</a>
+        </div>
+        <div class="card-body">
+            <table class="table table-bordered table-striped">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Người tạo đơn</th>
+                    <th scope="col">Tên khách hàng</th>
+                    <th scope="col">Ngày đặt hàng</th>
+                    <th scope="col" width='35%'>Chức năng</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php
+                foreach ($listOrders as $order) {
+                    echo "<tr>";
+                    echo "<td>" . $order['id'] . "</td>";
+
+                    $userId = $order['user_id'];
+
+                    $userOrder = $user->getUserById($userId);
+                    $name = $userOrder['username'];
+                    echo "<td>" . $name . "</td>";
+                    echo "<td>" . $order['customer_name'] . "</td>";
+
+                    echo "<td>" . $order['order_date'] . "</td>";
+                    echo "<td class='d-flex justify-content-evenly'>";
+                    echo "<a href='./index.php?act=listorderdetail&id=" . $order['id'] . "' class='btn btn-info text-white'>Xem thông tin chi tiết</a>";
+                    echo "<a href='./index.php?act=updateorder&id=" . $order['id'] . "' class='btn btn-warning text-white'>Cập nhật</a>";
+                    echo "<button type='button' onclick='confirmDelete(" . $order['id'] . ")' class='btn btn-danger text-white'>Xoá</button>";
+                    echo "</td>";
+                    echo "</tr>";
+                }
+                ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+<script>
+    function confirmDelete(id) {
+        Swal.fire({
+            title: 'Bạn chắc chắn muốn xoá?',
+            text: 'Hành động này sẽ xoá vĩnh viễn dữ liệu của đơn hàng này!',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Có, xoá!',
+            cancelButtonText: 'Không, hủy bỏ'
+        }).then((result) => {
+            if (result.value) {
+                window.location.href = './index.php?act=deleteorder&id=' + id;
+            }
+        });
+    }
+</script>
