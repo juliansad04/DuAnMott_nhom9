bc51ad376cf57023437349938c3e557ccc4620ec
thanhnppc <thanhnppc05965@fpt.edu.vn>
2023-11-24 11:09:37 +0700

    N9_admin_34:include_comment

--- admin/include/footer.php
+++ admin/include/footer.php
@@ -1,1 +1,6 @@
-<?php
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
