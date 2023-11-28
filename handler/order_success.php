d100d837bd1b1b379337f72a8014790ae8211f2f
thanhnppc <thanhnppc05965@fpt.edu.vn>
2023-11-28 08:12:43 +0700

    N9_client_15:order_success

---
+++ handler/order_success.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Thông báo đặt hàng thành công</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="alert alert-success text-center" role="alert">
+                    <div class="d-flex flex-column align-items-center mb-3">
+                        <i class="fas fa-check-circle fa-5x mb-3"></i>
+                        <h1 class="alert-heading">Đặt hàng thành công!</h1>
+                        <p>Cảm ơn bạn đã đặt hàng. Đơn hàng của bạn đã được ghi nhận.</p>
+                    </div>
+                    <div class="d-flex justify-content-center">
+                        <a href="../taikhoan.php" class="payment-success btn btn-primary mr-2">Xem chi tiết
+                            đơn hàng</a>
+                        <a href="../index.php" class="btn btn-secondary">Tiếp tục mua sắm</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
