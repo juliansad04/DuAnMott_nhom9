d09e8c660d7b839c4b94a1db68c83b3cafea36b1
thanhnppc <thanhnppc05965@fpt.edu.vn>
2023-11-28 08:13:05 +0700

    N9_client_16:post_comment

---
+++ handler/post_comment.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+include('../admin/include/pdo.php');
+include('../admin/comment/comment.php');
+$comment = new Comment();
+
+if (isset($_POST['post_comment'])) {
+    $product_id = $_POST['product_id'];
+    $user_id = $_SESSION['id'];
+    $comment_text = $_POST['comment'];
+    $comment_date = date("Y-m-d H:i:s");
+    $comment->insertComment($product_id, $user_id, $comment_text, $comment_date);
+}
+
+header("Location: ../chitietsp.php?id=$product_id"); 
+exit();
+?>
