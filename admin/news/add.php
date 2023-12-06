c196004ee029d9d90507c0989c2e429b8465fa0b
devkhanh <huekha0.4@gmail.com>
2023-12-06 23:04:30 +0700

    update_all

--- admin/news/add.php
+++ admin/news/add.php
@@ -1,77 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Thêm mới tin tức</title>
-    <!-- Thêm link CSS của Bootstrap -->
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
-</head>
-
-<body>
-
 <div class="container mt-5">
     <h2 class="mb-4">Thêm tin</h2>
 
     <form method="post" enctype="multipart/form-data" onsubmit="return validateNew()">
         <div class="form-group">
             <label for="title_news">Tiêu đề</label>
-            <input type="text" class="form-control" id="title_news" name="title_news" >
+            <input type="text" class="form-control" id="title_news" name="title_news">
+            <span id="titleError" class="text-danger"></span>
         </div>
         <div class="form-group">
             <label for="img_news">Hình ảnh</label>
             <input type="file" class="form-control-file" id="img_news" name="img_news" accept="image/*">
+            <span id="imgError" class="text-danger"></span>
         </div>
         <div class="form-group">
             <label for="content_news">Nội dung</label>
-            <div id="editor">
-            </div>
+            <div id="editor"></div>
             <input type="hidden" id="content_news" name="content_news">
+            <span id="contentError" class="text-danger"></span>
         </div>
         <a href="index.php?act=listnews" type="button" class="btn btn-danger">Hủy</a>
         <button class="btn btn-primary" name="addNews">Thêm</button>
     </form>
     <script src="/validate/validatenews.js"></script>
-
 </div>
-
-<!-- Thêm link JavaScript của Bootstrap (nếu cần) -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-
-</body>
-<script>
-    var options = {
-        modules: {
-            toolbar: [
-                ['bold', 'italic', 'underline', 'strike'],
-                [{
-                    'header': [1, 2, 3, 4, 5, 6, false]
-                }],
-                ['link', 'image', 'video'],
-                [{
-                    'list': 'ordered'
-                }, {
-                    'list': 'bullet'
-                }],
-                ['blockquote', 'code-block'],
-                [{
-                    'align': []
-                }],
-                ['clean']
-            ]
-        },
-        placeholder: 'Nhập mô tả sản phẩm...',
-        theme: 'snow'
-    };
-
-    var quill = new Quill('#editor', options);
-    document.querySelector('form').addEventListener('submit', function() {
-        var content_newsn = document.getElementById('content_news');
-        content_news.value = quill.root.innerHTML;
-    });
-</script>
-
-</html>
