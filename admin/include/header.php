51c6e51d5815944d726c8cac5c83d7688ac08c75
devkhanh <huekha0.4@gmail.com>
2023-12-16 14:52:19 +0700

    update_header

--- admin/include/header.php
+++ admin/include/header.php
@@ -190,7 +190,13 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                     <!-- lưu thông tin login -->
-                    Tên admin
+                    <?php
+                    if (isset($_SESSION['admin']) && !empty($_SESSION['admin'])) {
+                        $username = $_SESSION['admin'];
+
+                        echo '<li class="nav-item"><a class="nav-link" href="#">Chào mừng: ' . $username . '</a></li>';
+                    }
+                    ?>
                 </ul>
             </div>
         </div>
